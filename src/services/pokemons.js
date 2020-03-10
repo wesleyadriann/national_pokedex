803f6f4e69f4459e4ac89a1dde0f9853f546f024
Wesley Adriann <wesleyadriann@gmail.com>
2020-03-10 17:44:20 -0300

    Updates request pokemons

--- src/services/pokemons.js
+++ src/services/pokemons.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 import config from '../config';
 
-export const getAllPokemons = () => (
+export const getAllPokemons = (page = 0, perPage = 20) => (
   new Promise((resolve, reject) => {
-    axios.get(`${config.ENDPOINT_POKEAPI}/?limit=965`)
+    axios.get(`${config.ENDPOINT_POKEAPI}/?offset=${page * perPage}&limit=${perPage})
       .then((res) => resolve(res.data))
       .catch((err) => reject(err));
   })
